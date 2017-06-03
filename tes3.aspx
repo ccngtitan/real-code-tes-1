03463cdec93817c5d0cff8bd7d893eca28b79922
ccngtitan <ccngtitan@gmail.com>
2017-06-03 18:22:03 +0800

    Add files via upload

---
+++ tes3.aspx
@@ -0,0 +1,11 @@
+<%@ Page Language="c#" AutoEventWireup="false" Debug="true" CodeFile="tes3.aspx.cs" Inherits="TestPage"%>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form runat=server>
+	<asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+</form>
+</body>
+</html>
---
+++ tes3.aspx.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class TestPage : System.Web.UI.Page
+{
+	private static readonly string did = "admint4123", dpw = "12345^Qa";
+	private SqlConnection con = new SqlConnection("Server=tcp:t4bsdev.database.windows.net,1433;Initial Catalog=MBPDB;Persist Security Info=False;User ID=" + did + ";Password=" + dpw + ";MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+	public string str = "";
+	
+    public TestPage()
+    {
+		
+	}
+}
